7f4b85522630db3f48a8b359c008e353fc4a350b
ankitnirwal <ankit@somish.com>
2021-02-28 11:51:58 +0530

    bind asset page

--- src/app/theme/pages/default/asset/asset.component.ts
+++ src/app/theme/pages/default/asset/asset.component.ts
@@ -4,6 +4,7 @@ import { ethers } from 'ethers';
 import { interval, Observable, Subscription, Subject } from 'rxjs';
 import { map } from 'rxjs/operators'
 import { Router } from "@angular/router";
+declare var $: any;
 @Component({
   selector: '.wrapper',
   templateUrl: './asset.component.html',
@@ -11,13 +12,31 @@ import { Router } from "@angular/router";
 export class AssetComponent implements OnInit, AfterViewInit {
   public web3 = window['ethereum'];
   public provider;
-  public tradeData = { "partyAddress": null, "nftTokenAddress": null, 'exchangeId': null, "expiry": null, "tokenAddress": null, "tokenName": null, "tokenAmount": null, "days": null, "hours": null, "minutes": null, "seconds": null };
+  public tradeData = { "nftAddress": null, "tokenId": null, "tokenValidity": null, "percentage": null, "categoryId": null, "desc": null, "partyAddress": null, "nftTokenAddress": null, 'exchangeId': null, "expiry": null, "tokenAddress": null, "tokenName": null, "tokenAmount": null, "days": null, "hours": null, "minutes": null, "seconds": null };
   public exchangeContractInstance;
   public $counter: Observable<number>;
   public subscription: Subscription;
   public exchangeId;
   public createTrade = false;
   public txSuccess = false;
+  public allEchangeIds = [];
+  public metamaskNotPresent = false;
+  public wrongNetwork = false;
+  public ownerNft = false;
+  public show = false;
+  public approving = false;
+  public buying = false;
+  public cancelling = false;
+  public creating = false;
+  public approvingCreate = false;
+  public topup = false;
+  public expiryDate;
+  public tokenAmount;
+  public error = false;
+  public topupAmount;
+  public validtill;
+  public approveTopUp = false;
+  public errorTopup = false;
   constructor(private router: Router) {
 
   }
@@ -26,45 +45,94 @@ export class AssetComponent implements OnInit, AfterViewInit {
     if (this.web3 != undefined) {
       if (this.web3.selectedAddress != null) {
         this.provider = new ethers.providers.Web3Provider(this.web3);
-        this.exchangeContractInstance = new ethers.Contract(environment.exchangeAddress, environment.exchangeAbi, this.provider);
+        this.provider.getNetwork().then(network => {
+          if (network.chainId != 42) {
+            this.wrongNetwork = true;
+            $("#modal-metamaskCheck1").modal("show");
+          } else {
+            this.exchangeContractInstance = new ethers.Contract(environment.exchangeAddress, environment.exchangeAbi, this.provider.getSigner());
+            this.availableTradeData();
+          }
+        });
       } else {
-        this.provider = new ethers.providers.WebSocketProvider(environment.jsonRpcUrl);
-        this.exchangeContractInstance = new ethers.Contract(environment.exchangeAddress, environment.exchangeAbi, this.provider);
+        $("#modal-metamaskCheck1").modal("show");
       }
     } else {
-      this.provider = new ethers.providers.WebSocketProvider(environment.jsonRpcUrl);
-      this.exchangeContractInstance = new ethers.Contract(environment.exchangeAddress, environment.exchangeAbi, this.provider);
+      this.metamaskNotPresent = true;
+      $("#modal-metamaskCheck1").modal("show");
     }
-    this.availableTradeData();
   }
 
   ngAfterViewInit(): void {
+    if (window['ethereum'] != undefined) {
+      window['ethereum'].on('accountsChanged', () => {
+        setTimeout(async () => {
+          window.location.reload();
+        })
+      });
 
+      window['ethereum'].on('networkChanged', () => {
+        setTimeout(async () => {
+          window.location.reload();
+        })
+      });
+    }
   }
 
   async availableTradeData() {
-    this.exchangeId = this.router.url.split('/')[2];
-    let nftDetails = await this.exchangeContractInstance.exchange(this.exchangeId);
-    if (nftDetails.status == 0) {
-      let tokenInstance = new ethers.Contract(nftDetails.token2, environment.ERC20_Token_Abi, this.provider);
-      let tokenName = await tokenInstance.name()
+    let data = await this.exchangeEventData();
+    for (let i = 0; i < data.length; i++) {
+      let exchangeId = parseInt(data[i].decodedLog.args._exchangeID);
+      let nftData = await this.exchangeContractInstance.exchange(exchangeId);
+      let rawData = {
+        tokenId: parseInt(nftData.tokenID),
+        exchangeId: exchangeId
+      }
+      this.allEchangeIds.push(rawData)
+    }
+    let tokenId = this.router.url.split('/')[2];
+    let exchangeId = this.allEchangeIds.filter(el => el.tokenId == tokenId);
+    if (exchangeId.length != 0) {
+      this.exchangeId = exchangeId[exchangeId.length - 1].exchangeId;
+      let nftDetails = await this.exchangeContractInstance.exchange(this.exchangeId);
+      if (nftDetails.party1.toLowerCase() == this.web3.selectedAddress.toLowerCase()) {
+        this.ownerNft = true;
+      }
+      let couponInstance = new ethers.Contract(environment.PlotxCouponAddress, environment.plotxCouponAbi, this.provider);
+      let categoryData = await couponInstance.couponCategory(nftDetails.tokenID);
+      let standardTime = new Date(parseInt(categoryData.validity) * 1000);
+      let time = standardTime.toString().substr(4, 12);
+      let timeFormat = this.formatAMPM(new Date(parseInt(categoryData.validity) * 1000));
+      this.tradeData["tokenValidity"] = time + " " + timeFormat
+      this.tradeData["categoryId"] = parseInt(categoryData.categoryId);
+      let descData = await couponInstance._defaultCouponType(categoryData.categoryId);
+      this.show = true;
+      this.tradeData["desc"] = descData.desc;
+      this.tradeData["percentage"] = descData.perc;
       this.tradeData["partyAddress"] = this.truncateAddress(nftDetails.party1);
+      this.tradeData["nftAddress"] = nftDetails.token1;
       this.tradeData["nftTokenAddress"] = this.truncateAddress(nftDetails.token1);
-      this.tradeData["exchangeId"] = parseInt(nftDetails.tokenID);
-      this.tradeData["expiry"] = parseInt(nftDetails.expiry);
-      this.tradeData["tokenAddress"] = nftDetails.token2;
-      this.tradeData["tokenName"] = tokenName;
-      this.tradeData["amount"] = nftDetails.amount2OrTokenID,
-        this.tradeData["tokenAmount"] = this.toDecimal(parseFloat(ethers.utils.formatEther(nftDetails.amount2OrTokenID)), 2);
-      let t = 0;
-      this.$counter = interval(1000).pipe(map((x) => {
-        let deadline = this.tradeData.expiry * 1000;
-        let now = new Date().getTime();
-        t = (deadline) - now;
-        return x;
-      }));
-      if (t >= 0) {
-        this.subscription = this.$counter.subscribe((x) => this.countDownTimer(+t));
+      this.tradeData["tokenId"] = parseInt(nftDetails.tokenID);
+      if (nftDetails.status == 0) {
+        let tokenInstance = new ethers.Contract(nftDetails.token2, environment.ERC20_Token_Abi, this.provider);
+        let tokenName = await tokenInstance.name()
+        this.tradeData["expiry"] = parseInt(nftDetails.expiry);
+        this.tradeData["tokenAddress"] = nftDetails.token2;
+        this.tradeData["tokenName"] = tokenName;
+        this.tradeData["amount"] = nftDetails.amount2OrTokenID,
+          this.tradeData["tokenAmount"] = this.toDecimal(parseFloat(ethers.utils.formatEther(nftDetails.amount2OrTokenID)), 2);
+        let t = 0;
+        this.$counter = interval(1000).pipe(map((x) => {
+          let deadline = this.tradeData.expiry * 1000;
+          let now = new Date().getTime();
+          t = (deadline) - now;
+          return x;
+        }));
+        if (t >= 0) {
+          this.subscription = this.$counter.subscribe((x) => this.countDownTimer(+t));
+        }
+      } else {
+        this.createTrade = true;
       }
     } else {
       this.createTrade = true;
@@ -115,27 +183,151 @@ export class AssetComponent implements OnInit, AfterViewInit {
 
   async buyNFT() {
     try {
-      let tokenInstance = new ethers.Contract(this.tradeData['tokenAddress'], environment.ERC20_Token_Abi, this.provider);
+      this.approving = true;
+      let tokenInstance = new ethers.Contract(this.tradeData['tokenAddress'], environment.ERC20_Token_Abi, this.provider.getSigner());
       let approve = await tokenInstance.approve(environment.exchangeAddress, this.tradeData["amount"]);
       await this.provider.waitForTransaction(approve.hash);
       await this.provider.waitForTransaction(approve.hash);
-      let buyNft = await this.exchangeContractInstance.party2Response(this.tradeData["exchangeId"]);
+      this.approving = false;
+      this.buying = true;
+      let buyNft = await this.exchangeContractInstance.party2Response(this.exchangeId);
       await this.provider.waitForTransaction(buyNft.hash);
       await this.provider.waitForTransaction(buyNft.hash);
       this.txSuccess = true;
+      this.buying = false;
     } catch (e) {
       console.log(e)
+      this.approving = false;
+      this.buying = false;
     }
   }
 
   async cancelTrade() {
     try {
-      let buyNft = await this.exchangeContractInstance.withdrawRequest(this.tradeData["exchangeId"]);
-      await this.provider.waitForTransaction(buyNft.hash);
-      await this.provider.waitForTransaction(buyNft.hash);
+      this.cancelling = true;
+      let cancelNft = await this.exchangeContractInstance.withdrawRequest(this.exchangeId);
+      await this.provider.waitForTransaction(cancelNft.hash);
+      await this.provider.waitForTransaction(cancelNft.hash);
+      this.cancelling = false;;
       this.txSuccess = true;
     } catch (e) {
+      this.cancelling = false;
       console.log(e)
     }
+  }
+
+  async createExchange() {
+    try {
+      let now = new Date().getTime();
+      let expiryTime = new Date(this.expiryDate).getTime();
+      let time = Math.floor((expiryTime - now) / 1000);
+      if (!this.tokenAmount || !this.expiryDate || time <= 0 || this.tokenAmount <= 0) {
+        this.error = true;
+      } else {
+        this.approvingCreate = true;
+        let tokenInstance = new ethers.Contract(environment.PlotxCouponAddress, environment.plotxCouponAbi, this.provider.getSigner());
+        let approve = await tokenInstance.approve(environment.exchangeAddress, this.tradeData["tokenId"]);
+        await this.provider.waitForTransaction(approve.hash);
+        await this.provider.waitForTransaction(approve.hash);
+        this.approvingCreate = false;
+        this.creating = true;
+        let create = await this.exchangeContractInstance.createExchange(this.tradeData["tokenId"],
+          this.tradeData["nftAddress"], time, environment.ERC20_Token, ethers.utils.parseEther(this.tokenAmount.toString()));
+        await this.provider.waitForTransaction(create.hash);
+        await this.provider.waitForTransaction(create.hash);
+        this.creating = false;
+        this.txSuccess = true;
+      }
+    } catch (e) {
+      this.creating = false;
+      this.approvingCreate = false;
+      console.log(e)
+    }
+  }
+
+  async topUp() {
+    try {
+      if (!this.topupAmount || this.topupAmount <= 0) {
+        this.errorTopup = true;
+      } else {
+        this.approveTopUp = true;
+        let tokenInstance = new ethers.Contract(environment.ERC20_Token, environment.ERC20_Token_Abi, this.provider.getSigner());
+        let approve = await tokenInstance.approve(environment.PlotxCouponAddress, ethers.utils.parseEther(this.topupAmount.toString()));
+        await this.provider.waitForTransaction(approve.hash);
+        await this.provider.waitForTransaction(approve.hash);
+        this.approveTopUp = false;
+        this.topup = true;
+        let couponInstance = new ethers.Contract(environment.PlotxCouponAddress, environment.plotxCouponAbi, this.provider.getSigner());
+        let create = await couponInstance.refillValidity(this.tradeData["tokenId"], ethers.utils.parseEther(this.topupAmount.toString()));
+        await this.provider.waitForTransaction(create.hash);
+        await this.provider.waitForTransaction(create.hash);
+        this.topup = false;
+        this.txSuccess = true;
+      }
+    } catch (e) {
+      this.topup = false;
+      this.approveTopUp = false;
+      console.log(e)
+    }
+  }
+
+  formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+  }
+
+  async exchangeEventData() {
+    // this.provider.resetEventsBlock(0)
+    let topic = ethers.utils.id("ExchangeCreated(uint256,address,address,uint256,address,uint256)");
+    var iface = new ethers.utils.Interface(environment.exchangeAbi);
+    let filter = {
+      address: environment.exchangeAddress,
+      fromBlock: 0,
+      toBlock: 'latest',
+      topics: [topic]
+    }
+    var callPromise = this.provider.getLogs(filter);
+    var eventData = callPromise.then(function (events) {
+      var parsedEvents = events.map(function (log) {
+        let blockNumber = log.blockNumber;
+        let decodedLog = iface.parseLog(log)
+        let iFaceData = {
+          decodedLog,
+          blockNumber
+        }
+        return iFaceData
+      });
+      return parsedEvents;
+    }).catch(function (err) {
+      console.log(err);
+    });
+    return eventData;
+  }
+
+  async enableMetamask() {
+    await this.web3.enable();
+    window.location.reload();
+  }
+
+  reload() {
+    window.location.reload()
+  }
+
+  removeError() {
+    this.error = false;
+  }
+
+  removeTopupError() {
+    this.errorTopup = false;
+  }
+
+  validTill() {
+    this.validtill = this.toDecimal((this.topupAmount * 100) / 3600, 2) + " hours";
   }
 }
--- src/app/theme/pages/default/index/index.component.ts
+++ src/app/theme/pages/default/index/index.component.ts
@@ -45,35 +45,48 @@ export class IndexComponent implements OnInit, AfterViewInit {
 
   async availableTradeData() {
     let data = await this.exchangeEventData();
-    for (let i = 0; i < data.length; i++) {
+    for (let i = data.length - 1; i >= 0; i--) {
       let exchangeId = parseInt(data[i].decodedLog.args._exchangeID);
       let nftDetails = await this.exchangeContractInstance.exchange(exchangeId);
-      console.log(nftDetails)
       if (nftDetails.status == 0) {
         let tokenInstance = new ethers.Contract(nftDetails.token2, environment.ERC20_Token_Abi, this.provider);
+        let couponInstance = new ethers.Contract(environment.PlotxCouponAddress, environment.plotxCouponAbi, this.provider);
+        let categoryData = await couponInstance.couponCategory(nftDetails.tokenID);
+        let categoryId = parseInt(categoryData.categoryId)
+        let validity = parseInt(categoryData.validity)
+        let descData = await couponInstance._defaultCouponType(categoryData.categoryId);
+        let desc = descData.desc;
         let tokenName = await tokenInstance.name()
+        let standardTime = new Date(validity * 1000);
+        let time = standardTime.toString().substr(4, 12);
+        let timeFormat = this.formatAMPM(new Date(validity * 1000));
         let availableTradeData = {
           partyAddress: this.truncateAddress(nftDetails.party1),
           nftTokenAddress: this.truncateAddress(nftDetails.token1),
-          exchangeId: parseInt(nftDetails.tokenID),
+          tokenId: parseInt(nftDetails.tokenID),
           expiry: parseInt(nftDetails.expiry),
-          tokenAddress : nftDetails.token2,
-          tokenName : tokenName,
+          tokenAddress: nftDetails.token2,
+          tokenName: tokenName,
+          tokenDesc: desc,
+          categoryId: categoryId,
+          tokenValidity: time + " " + timeFormat,
           tokenAmount: this.toDecimal(parseFloat(ethers.utils.formatEther(nftDetails.amount2OrTokenID)), 2)
         }
         this.tradeData.push(availableTradeData)
-        let t = 0;
-        this.$counter = interval(1000).pipe(map((x) => {
-          let deadline = availableTradeData.expiry * 1000;
-          let now = new Date().getTime();
-          t = (deadline) - now;
-          return x;
-        }));
-        if (t >= 0) {
-          this.subscription = this.$counter.subscribe((x) => this.countDownTimer(i, +t));
-        }
       }
     }
+    for(let j=0;j<this.tradeData.length;j++){
+      let t = 0;
+      this.$counter = interval(1000).pipe(map((x) => {
+        let deadline = this.tradeData[j].expiry * 1000;
+        let now = new Date().getTime();
+        t = (deadline) - now;
+        return x;
+      }));
+      if (t >= 0) {
+        this.subscription = this.$counter.subscribe((x) => this.countDownTimer(j, +t));
+      }
+  }
   }
 
   async exchangeEventData() {
@@ -144,5 +157,16 @@ export class IndexComponent implements OnInit, AfterViewInit {
         this.tradeData[i]['seconds'] = "0" + this.tradeData[i]['seconds'];
       }
     }
+  }
+
+  formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
   }
 }
